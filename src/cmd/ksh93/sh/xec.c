ksh93/ksh

commit f762a81a822c20ef30897e2c3921802d77b1b141

libast: spawnveg(3): update for glibc 2.35 (re: 8e9ed5be) (#450)
Version 2.35 of glibc has recently been released[1]. Shortly before
the release, posix_spawnattr_tcsetpgrp_np was replaced by
posix_spawn_file_actions_addtcsetpgrp_np()[2]. This commit updates
spawnveg(3) to use the new function instead for setting the
terminal group.

[1]: https://sourceware.org/pipermail/libc-alpha/2022-February/136040.html
[2]: https://sourceware.org/git/?p=glibc.git;a=commit;h=6289d28d3c4e56f34830cfb011c31271ef850418

src/lib/libast/comp/spawnveg.c,
src/lib/libast/man/spawnveg.3:
- Allow spawnveg to set the terminal process group when pgid == 0.
  This was necessary to avoid race conditions when using the new
  function.
- Updated the man page to account for spawnveg's change in
  behavior.

src/lib/libast/features/api,
src/cmd/ksh93/include/defs.h:
- Increment libast version since spawnveg now doesn't ignore tcfd
  if pgid is zero.

src/cmd/ksh93/sh/xec.c
@@ -55,7 +55,7 @@
 #   include <sys/resource.h>
 #endif
 
-#if _lib_posix_spawn > 1 && _lib_posix_spawnattr_tcsetpgrp_np
+#if _lib_posix_spawn > 1 && _lib_posix_spawn_file_actions_addtcsetpgrp_np
 #define _use_ntfork_tcpgrp 1
 #endif
 
@@ -3453,7 +3453,7 @@ static void sigreset(int mode)
 /*
  * A combined fork/exec for systems with slow fork().
  * Incompatible with job control on interactive shells (job.jobcontrol) if
- * the system does not support posix_spawnattr_tcsetpgrp_np().
+ * the system does not support posix_spawn_file_actions_addtcsetpgrp_np().
  */
 static pid_t sh_ntfork(const Shnode_t *t,char *argv[],int *jobid,int flag)
 {

src/lib/libast/comp/spawnveg.c
@@ -41,25 +41,21 @@
 pid_t
 spawnveg(const char* path, char* const argv[], char* const envv[], pid_t pgid, int tcfd)
 {
-	int			err, flags = 0;
-	pid_t			pid;
-	posix_spawnattr_t	attr;
+	int				err, flags = 0;
+	pid_t				pid;
+	posix_spawnattr_t		attr;
+#if _lib_posix_spawn_file_actions_addtcsetpgrp_np
+	posix_spawn_file_actions_t	actions;
+#endif
 
 	if (err = posix_spawnattr_init(&attr))
 		goto nope;
 #if POSIX_SPAWN_SETSID
 	if (pgid == -1)
 		flags |= POSIX_SPAWN_SETSID;
-	else
 #endif
-	if (pgid)
-	{
+	if (pgid && pgid != -1)
 		flags |= POSIX_SPAWN_SETPGROUP;
-#if _lib_posix_spawnattr_tcsetpgrp_np
-		if (tcfd >= 0)
-			flags |= POSIX_SPAWN_TCSETPGROUP;
-#endif
-	}
 	if (flags && (err = posix_spawnattr_setflags(&attr, flags)))
 		goto bad;
 	if (pgid && pgid != -1)
@@ -68,18 +64,34 @@ spawnveg(const char* path, char* const argv[], char* const envv[], pid_t pgid, i
 			pgid = 0;
 		if (err = posix_spawnattr_setpgroup(&attr, pgid))
 			goto bad;
-#if _lib_posix_spawnattr_tcsetpgrp_np
-		if (tcfd >= 0 && (err = posix_spawnattr_tcsetpgrp_np(&attr, tcfd)))
-			goto bad;
-#endif
 	}
+#if _lib_posix_spawn_file_actions_addtcsetpgrp_np
+	if (tcfd >= 0)
+	{
+		if (err = posix_spawn_file_actions_init(&actions))
+			goto bad;
+		if (err = posix_spawn_file_actions_addtcsetpgrp_np(&actions, tcfd))
+			goto fail;
+	}
+	if (err = posix_spawn(&pid, path, (tcfd >= 0) ? &actions : NiL, &attr, argv, envv ? envv : environ))
+#else
 	if (err = posix_spawn(&pid, path, NiL, &attr, argv, envv ? envv : environ))
+#endif
 	{
 		if ((err != EPERM) || (err = posix_spawn(&pid, path, NiL, NiL, argv, envv ? envv : environ)))
-			goto bad;
+			goto fail;
 	}
+#if _lib_posix_spawn_file_actions_addtcsetpgrp_np
+	if (tcfd >= 0)
+		posix_spawn_file_actions_destroy(&actions);
+#endif
 	posix_spawnattr_destroy(&attr);
 	return pid;
+ fail:
+#if _lib_posix_spawn_file_actions_addtcsetpgrp_np
+	if (tcfd >= 0)
+		posix_spawn_file_actions_destroy(&actions);
+#endif
  bad:
 	posix_spawnattr_destroy(&attr);
  nope:

src/lib/libast/features/lib
@@ -343,7 +343,7 @@ tst	lib_posix_spawn unistd.h stdlib.h spawn.h -Dfork=______fork note{ posix_spaw
 		_exit(n);
  	}
 }end
-lib	posix_spawnattr_tcsetpgrp_np
+lib	posix_spawn_file_actions_addtcsetpgrp_np
 
 tst	lib_spawn_mode unistd.h stdlib.h note{ first spawn arg is mode and it works }end execute{
 	#include <signal.h>

src/lib/libast/man/spawnveg.3
@@ -85,21 +85,17 @@ The new process joins the process group
 The
 .L tcfd
 argument is currently only used if the operating system supports the
-.I posix_spawnattr_tcsetpgrp_np
+.I posix_spawn_file_actions_addtcsetpgrp_np
 function.
 When
 .L tcfd
 is
-.L >=0
-and
-.L pgid
-is
-.LR >0 ,
+.LR >=0 ,
 spawnveg will set the controlling terminal for the new process to
 .IR tcfd .
 .SH CAVEATS
 If the
-.I posix_spawnattr_tcsetpgrp_np
+.I posix_spawn_file_actions_addtcsetpgrp_np
 function is not available, then
 .L spawnveg
 cannot reliably set the terminal process group.

commit f9b7992b240a16c8e5d101adb874b9e955e5e6e3

Fix infinite loop when posix_spawn fails (re: f762a81a)
This commit fixes an infinite loop introduced in commit f762a81a
that caused ksh to enter an infinite loop if posix_spawn failed
to start a new process after setting the terminal process group.
Reproducer (warning: it will cause ksh to crash Wayland sessions
and drives up CPU usage by a ton):
   $ /tmp/this/file/does/not/exist
   /usr/bin/ksh: /tmp/this/file/does/not/exist: not found
   $ <Press enter>
   (ksh now prints $PS1 in a loop until killed with SIGKILL)

src/cmd/ksh93/sh/xec.c: sh_ntfork():
- Restore the terminal process group if posix_spawn failed to launch
  a new process. This is necessary because posix_spawn will set the
  terminal process group before it attempts to run a command and
  doesn't restore it on failure.

src/cmd/ksh93/sh/xec.c
@@ -3593,19 +3593,26 @@ static pid_t sh_ntfork(const Shnode_t *t,char *argv[],int *jobid,int flag)
 	fail:
 		if(jobfork && spawnpid<0) 
 			job_fork(-2);
-		if(spawnpid == -1) switch(errno=sh.path_err)
+		if(spawnpid == -1)
 		{
-		    case ENOENT:
-			errormsg(SH_DICT,ERROR_exit(ERROR_NOENT),e_found+4);
-			UNREACHABLE();
+#if _use_ntfork_tcpgrp
+			if(job.jobcontrol)
+				tcsetpgrp(job.fd,job.mypgid);
+#endif /* _use_ntfork_tcpgrp */
+			switch(errno=sh.path_err)
+			{
+			    case ENOENT:
+				errormsg(SH_DICT,ERROR_exit(ERROR_NOENT),e_found+4);
+				UNREACHABLE();
 #ifdef ENAMETOOLONG
-		    case ENAMETOOLONG:
-			errormsg(SH_DICT,ERROR_exit(ERROR_NOENT),e_toolong+4);
-			UNREACHABLE();
+			    case ENAMETOOLONG:
+				errormsg(SH_DICT,ERROR_exit(ERROR_NOENT),e_toolong+4);
+				UNREACHABLE();
 #endif
-		    default:
-			errormsg(SH_DICT,ERROR_system(ERROR_NOEXEC),e_exec+4);
-			UNREACHABLE();
+			    default:
+				errormsg(SH_DICT,ERROR_system(ERROR_NOEXEC),e_exec+4);
+				UNREACHABLE();
+			}
 		}
 		job_unlock();
 	}