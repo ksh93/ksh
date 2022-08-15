ksh93/ksh

commit 4b445059788aeeadd8ff9940263140d6235a0e56

libast/sfio: remove unused Vthread API and support code
Sfio may theoretically be compiled with threads support using a
separate AT&T library called Vthread, also by Kiem-Phong Vo. That
library was never shipped in the AST distribution, though. It is
only available with a standalone version of Sfio.

The only standalone Sfio version with Vthread that I've found is
from 2005, mirrored at <https://github.com/lichray/sfio>. More
recent versions never seem to have made it out of the defunct AT&T
software download site.

Even if they weren't, the rest of libast doesn't support threads,
and at this point it never will, so for our purposes the Sfio
threads code is never going to be usable. Meanwhile, macros such as
SFMTXENTER and SFMTXRETURN make the code a lot harder to read. And
not quite all threading code is disabled; some of it is dead code
that is getting compiled in.

Chances are that code now won't work properly in any case as we've
not had any chance to test it as we were making changes. Bit rot
has surely set in by now.

So this commit expands all the sfio/stdio threading-related macros
to their non-threads fallbacks (which is null for most of them, but
not all), deletes dead mutex-related code and struct fields, and
removes the related documentation from the sfio.3 man page. Unless
I did something wrong, there should be no change in behaviour.

src/lib/libast/sfio/sfmove.c
@@ -2,7 +2,7 @@
 *                                                                      *
 *               This software is part of the ast package               *
 *          Copyright (c) 1985-2011 AT&T Intellectual Property          *
-*          Copyright (c) 2020-2021 Contributors to ksh 93u+m           *
+*          Copyright (c) 2020-2022 Contributors to ksh 93u+m           *
 *                      and is licensed under the                       *
 *                 Eclipse Public License, Version 1.0                  *
 *                    by AT&T Intellectual Property                     *
@@ -43,12 +43,9 @@ Sfoff_t sfmove(Sfio_t*	fr,	/* moving data from this stream */
 	Sfoff_t		n_move, sk, cur;
 	uchar		*rbuf = NIL(uchar*);
 	ssize_t		rsize = 0;
-	SFMTXDECL(fr);	/* declare a shadow stream variable for from stream */
-	SFMTXDECL2(fw);	/* declare a shadow stream variable for to stream */
 
-	SFMTXENTER(fr, (Sfoff_t)0);
+	if(!(fr)) return (Sfoff_t)0;
 	if(fw)
-		SFMTXBEGIN2(fw, (Sfoff_t)0);
 
 	for(n_move = 0; n != 0; )
 	{
@@ -234,9 +231,8 @@ Sfoff_t sfmove(Sfio_t*	fr,	/* moving data from this stream */
 
 	if(fw)
 	{	SFOPEN(fw,0);
-		SFMTXEND2(fw);
 	}
 
 	SFOPEN(fr,0);
-	SFMTXRETURN(fr, n_move);
+	return n_move;
 }

commit 823c4f6b4f3a7fb1c8761e5f2f4f6949b73e7f35

Fix regression in handling of head builtin's -s flag (re: 4b445059)
Commit 4b445059 accidentally left in half of an if statement that was
originally just a use of the SFMTXBEGIN2 macro (which is a no-op without
vt_threaded). As a result, the head builtin's -s flag broke in one of
the ksh2020 regression tests. Reproducer:
   # Note that head must be enabled in src/cmd/ksh93/data/builtins.c
   builtin head || exit 1
   cat > "/tmp/file1" <<EOF
This is line 1 in file1
This is line 2 in file1
This is line 3 in file1
This is line 4 in file1
This is line 5 in file1
This is line 6 in file1
This is line 7 in file1
This is line 8 in file1
This is line 9 in file1
This is line 10 in file1
This is line 11 in file1
This is line 12 in file1
EOF
   got=$(head -s 5 -c 18 "/tmp/file1")
   exp=$'is line 1 in file1'
   [[ "$got" = "$exp" ]] || print "'head -s' failed" "(expected $(printf %q "$exp"), got $(printf %q "$got"))"

Code change that caused this bug (note that since the if statement
wasn't completely removed, it now guards the for loop despite the
newline):
        if(fw)
-               SFMTXBEGIN2(fw, (Sfoff_t)0);

        for(n_move = 0; n != 0; )

I noticed this since I'm making a separate commit to backport more of
the ksh2020 regression tests.

src/lib/libast/sfio/sfmove.c:
- Remove the incomplete if statement to fix the -s flag.

src/cmd/ksh93/tests/b_head.sh:
- Backport the ksh2020 regression tests for the head builtin.

src/lib/libast/sfio/sfmove.c
@@ -14,6 +14,7 @@
 *                  David Korn <dgk@research.att.com>                   *
 *                   Phong Vo <kpv@research.att.com>                    *
 *                  Martijn Dekker <martijn@inlv.org>                   *
+*            Johnothan King <johnothanking@protonmail.com>             *
 *                                                                      *
 ***********************************************************************/
 #include	"sfhdr.h"
@@ -41,7 +42,6 @@ Sfoff_t sfmove(Sfio_t*	fr,	/* moving data from this stream */
 	ssize_t		rsize = 0;
 
 	if(!(fr)) return (Sfoff_t)0;
-	if(fw)
 
 	for(n_move = 0; n != 0; )
 	{

src/cmd/ksh93/tests/b_head.sh
@@ -1,0 +1,92 @@
+########################################################################
+#                                                                      #
+#               This software is part of the ast package               #
+#           Copyright (c) 2019-2020 Contributors to ksh2020            #
+#             Copyright (c) 2022 Contributors to ksh 93u+m             #
+#                      and is licensed under the                       #
+#                 Eclipse Public License, Version 2.0                  #
+#                                                                      #
+#                A copy of the License is available at                 #
+#      https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.html      #
+#         (with md5 checksum 84283fa8859daf213bdda5a9f8d1be1d)         #
+#                                                                      #
+#                 Kurtis Rader <krader@skepticism.us>                  #
+#            Johnothan King <johnothanking@protonmail.com>             #
+#                                                                      #
+########################################################################
+
+# Tests for `head` builtin
+
+. "${SHTESTS_COMMON:-${0%/*}/_common}"
+if ! builtin head 2> /dev/null; then
+	warning 'Could not detect head builtin; skipping tests'
+	exit 0
+fi
+
+cat > "$tmp/file1" <<EOF
+This is line 1 in file1
+This is line 2 in file1
+This is line 3 in file1
+This is line 4 in file1
+This is line 5 in file1
+This is line 6 in file1
+This is line 7 in file1
+This is line 8 in file1
+This is line 9 in file1
+This is line 10 in file1
+This is line 11 in file1
+This is line 12 in file1
+EOF
+
+cat > "$tmp/file2" <<EOF2
+This is line 1 in file2
+This is line 2 in file2
+This is line 3 in file2
+This is line 4 in file2
+This is line 5 in file2
+EOF2
+
+# ==========
+# -*n*; i.e., an integer presented as a flag.
+#
+# The `awk` invocation is to strip whitespace around the output of `wc` since it might pad the
+# value.
+exp=11
+got=$(head -11 < "$tmp/file1" | wc -l | awk '{ print $1 }')
+[[ "$got" = "$exp" ]] || err_exit "'head -n' failed" "(expected $(printf %q "$exp"), got $(printf %q "$got"))"
+
+# ==========
+#   -n, --lines=lines
+#                   Copy lines lines from each file. The default value is 10.
+got=$(head -n 3 "$tmp/file1")
+exp=$'This is line 1 in file1\nThis is line 2 in file1\nThis is line 3 in file1'
+[[ "$got" = "$exp" ]] || err_exit "'head -n' failed" "(expected $(printf %q "$exp"), got $(printf %q "$got"))"
+
+# ==========
+#   -c, --bytes=chars
+#                   Copy chars bytes from each file.
+got=$(head -c 14 "$tmp/file1")
+exp=$'This is line 1'
+[[ "$got" = "$exp" ]] || err_exit "'head -c' failed" "(expected $(printf %q "$exp"), got $(printf %q "$got"))"
+
+# ==========
+#   -q, --quiet|silent
+#                   Never output filename headers.
+got=$(head -q -n 3 "$tmp/file1" "$tmp/file2")
+exp=$'This is line 1 in file1\nThis is line 2 in file1\nThis is line 3 in file1\nThis is line 1 in file2\nThis is line 2 in file2\nThis is line 3 in file2'
+[[ "$got" = "$exp" ]] || err_exit "'head -q' failed" "(expected $(printf %q "$exp"), got $(printf %q "$got"))"
+
+# ==========
+#   -s, --skip=char Skip char characters or lines from each file before copying.
+got=$(head -s 5 -c 18 "$tmp/file1")
+exp=$'is line 1 in file1'
+[[ "$got" = "$exp" ]] || err_exit "'head -s' failed" "(expected $(printf %q "$exp"), got $(printf %q "$got"))"
+
+# ==========
+#   -v, --verbose   Always output filename headers.
+got=$(head -v -n 3 "$tmp/file1")
+exp=$'file1 <==\nThis is line 1 in file1\nThis is line 2 in file1\nThis is line 3 in file1'
+[[ "$got" =~ "$exp" ]] || err_exit "'head -v' failed" "(expected $(printf %q "$exp"), got $(printf %q "$got"))"
+
+# ======
+exit $((Errors<125?Errors:125))