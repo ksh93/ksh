1e04fb1ca6363738fe612972be9a29456436cf96
Johnothan King <johnothanking@protonmail.com>
2022-09-10 14:00:16 -0700

    Implement backwards reverse searches
    This commit implements the ability to do a backwards reverse search when
    using the down arrow key in the emacs and vi editing modes. It also
    implements the ability to do reverse searches with the Page Up and Page
    Down keys. Previously the only arrow key that performed reverse searches
    was the up arrow, which could result in confusing behavior:
       $ test foo
       $ false
       $ test bar
       $ te <Up Arrow * 2> <Down Arrow>
       $ false
    In the above example, using the down arrow after using the up arrow
    twice results in ksh returning 'false' rather than 'test bar'. This
    makes no sense, as you would expect both arrow keys to either perform
    a reverse search or no reverse search.
    
    src/cmd/ksh93/edit/{emacs,vi}.c:
    - Move code for preparing a reverse search to a 'dosearch()' function
      that is called for arrow key reverse searches.
    - In emacs mode, perform a backwards reverse search if ep->prevdirection
      is set to -3. This allows search() to tell hist_find() to search
      backwards in the history file.
    - In vi mode, backwards reverse searches were already available with
      'N', so set the mode to 'N' in dosearch() when performing backwards
      reverse searches.
    - Add support for performing reverse searches to the Page Up and Page
      Down keys (this is similar to how mksh handles these keys, though not
      identical). In emacs mode this allows backwards reverse searches to be
      performed after using ^R to search.
    - vi mode fix: Stop reverse searches after an interrupt (^C) is
      received. Reproducer for the fixed bug:
         $ test bar=foo
         $ sleep 0
         $ test foo=bar
         $ sleep 0.0
         $ te <Up Arrow*2> + <Ctrl+C)
         $ <Up Arrow*3>  # Should be 'sleep 0', is actually 'test bar=foo'
      In the above example, the reverse search is still active, so using
      the up arrow key will result in a reverse search. To fix this issue,
      vp->direction is set to -1 in dosearch() to cancel the reverse search
      (a value of -2 is used to indicate an active reverse search).
    
    src/cmd/ksh93/tests/pty.sh:
    - Added four regression tests for reverse searching with the arrow keys.
      Three of these tests are applicable to this commit, while one of them
      is a regression test for an emacs mode bug that was fixed in commit
      b5e52703.
    
    Resolves: https://github.com/ksh93/ksh/issues/444

--- src/cmd/ksh93/tests/pty.sh
+++ src/cmd/ksh93/tests/pty.sh
@@ -1045,5 +1045,76 @@ w echo @ !non_existent
 u @ !non_existent\r\n$
 !
 
+((SHOPT_VSH)) && tst $LINENO <<"!"
+L reverse search isn't canceled after an interrupt in vi mode
+
+d 15
+p :test-1:
+w echo WRONG
+p :test-2:
+w print CORREC
+p :test-3:
+w echo foo
+p :test-4:
+w sleep 0
+p :test-5:
+c e\E[A\E[A\cC\E[A\E[A\E[A
+w $aT
+u CORRECT
+!
+
+((SHOPT_ESH)) && VISUAL=emacs tst $LINENO <<"!"
+L failure to start new reverse search in emacs mode
+# https://github.com/ksh93/ksh/commit/b5e52703
+
+d 15
+p :test-1:
+w echo WRON
+p :test-2:
+w print CORREC
+p :test-3:
+w e\E[AG
+u WRONG
+p :test-4:
+w p\E[AT
+u CORRECT
+!
+
+((SHOPT_VSH)) && tst $LINENO <<"!"
+L backwards reverse search in vi mode
+
+d 15
+p :test-1:
+w print bar
+p :test-2:
+w print foo
+p :test-3:
+w false BAD ENTRY
+p :test-4:
+w print Correc
+p :test-5:
+c p\E[A\E[A\E[A\E[B\E[B
+w $at
+u Correct
+!
+
+((SHOPT_ESH)) && VISUAL=emacs tst $LINENO <<"!"
+L backwards reverse search in emacs mode
+
+d 15
+p :test-1:
+w print bar
+p :test-2:
+w print foo
+p :test-3:
+w false BAD ENTRY
+p :test-4:
+w print Correc
+p :test-5:
+c p\E[A\E[A\E[A\E[B\E[B
+w t
+u Correct
+!
+
 # ======
 exit $((Errors<125?Errors:125))
