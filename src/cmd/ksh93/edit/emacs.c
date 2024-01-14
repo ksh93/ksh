ksh93/ksh

commit 5eeeed066cd6ac9eac1b2f604e4ea3866869c4e9

Fix multiple bugs in emacs and vi (#565)
This (somewhat expanded) commit addresses three behaviors in vi
mode and two behaviors in emacs mode:

- In vi mode, calling v from a completely empty line rings the bell
  rather than calling the full editor.
- In vi mode, calling v from a spaces-only line results in an
  "invalid range" hist error, and the editor fails to open.
- In emacs mode, calling the full editor saves the current contents
  of the command line to the history file without a new line.
  (issue #563)
- In both editing modes, navigating to a null or empty history line
  displays an empty line onscreen.

A summary of the fixes:

The hist builtin seems to expect either a new line with the cursor
in position 0 or a line with at least one non-space character; in
all cases, the line should conclude with \n and \0:

- In vi.c, set the position to 0, refresh and insert \n if command
  mode v is called from an empty line or a line made up entirely of
  spaces. This enables hist to call the full editor with an
  appropriate range.
- In emacs.c, add \n and \0 to drawbuff before calling ed_fulledit
  (but after checking its failure conditions). This prevents the
  insertion of null history items for both empty and non-empty
  lines. Because lines composed entirely of spaces can cause the
  following prompt to be offset, clear such lines as well.

Other shells, including mksh, oksh, bash and yash, also save empty
history lines. However, actually displaying these blank lines when
navigating using e.g. the arrow keys (as is the case in ksh, mksh
and oksh) is unhelpful:

- In vi.c and emacs.c, check the history line for non-space
  characters. If the line is completely empty, resend the
  appropriate key provided that doing so would not go beyond the
  confines of the first and last history lines. This effectively
  skips empty lines when navigating.

Another bugfix:

Attempting to tab-complete an alias or $PATH item containing wide

src/cmd/ksh93/edit/emacs.c
@@ -71,6 +71,9 @@ One line screen editor for any program
 #include	"history.h"
 #include	"edit.h"
 #include	"terminal.h"
+#if SHOPT_MULTIBYTE
+#include	<wctype.h>
+#endif /* SHOPT_MULTIBYTE */
 
 #define ESH_NFIRST
 #define ESH_KAPPEND
@@ -175,12 +178,14 @@ static void search(Emacs_t*,genchar*,int);
 static void setcursor(Emacs_t*,int, int);
 static void show_info(Emacs_t*,const char*);
 static void xcommands(Emacs_t*,int);
+static char allwhitespace(Emacs_t*, genchar*);
 
 int ed_emacsread(void *context, int fd,char *buff,int scend, int reedit)
 {
 	Edit_t *ed = (Edit_t*)context;
 	register int c;
 	register int i;
+	register int uparrow;
 	register genchar *out;
 	register int count;
 	register Emacs_t *ep = ed->e_emacs;
@@ -645,6 +650,7 @@ update:
 				continue;
 #endif
 			}
+			uparrow = 1;
 			goto common;
 
 		case cntl('O') :
@@ -671,6 +677,7 @@ update:
 			}
 			hline = location.hist_command;
 			hloff = location.hist_line;
+			uparrow = 0;
 		common:
 #ifdef ESH_NFIRST
 			location.hist_command = hline;	/* save current position */
@@ -686,6 +693,13 @@ update:
 			eol = genlen(out);
 			cur = eol;
 			draw(ep,UPDATE);
+			if(allwhitespace(ep,out))
+			{
+				if(!uparrow && hline != histlines)
+					ed_ungetchar(ep->ed,cntl('N'));
+				if(uparrow && hline != hismin)
+					ed_ungetchar(ep->ed,cntl('P'));
+			}
 			continue;
 		}
 	}
@@ -951,18 +965,7 @@ static int escape(register Emacs_t* ep,register genchar *out,int count)
 		case '*':		/* filename expansion */
 		case '=':	/* escape = - list all matching file names */
 		{
-			char allempty = 1;
-			int x;
-			ep->mark = cur;
-			for(x=0; x < cur; x++)
-			{
-				if(!isspace(out[x]))
-				{
-					allempty = 0;
-					break;
-				}
-			}
-			if(cur<1 || allempty)
+			if(cur<1 || allwhitespace(ep,out))
 			{
 				beep();
 				return(-1);
@@ -1233,6 +1236,21 @@ static void xcommands(register Emacs_t *ep,int count)
 
 #ifdef ESH_BETTER
                 case cntl('E'):	/* invoke emacs on current command */
+			if(eol>=0 && sh.hist_ptr)
+			{
+				if(allwhitespace(ep,drawbuff))
+				{
+					cur = 0;
+					eol = 1;
+					drawbuff[cur] = '\n';
+					drawbuff[eol] = '\0';
+				}
+				else
+				{
+					drawbuff[eol] = '\n';
+					drawbuff[eol+1] = '\0';
+				}
+			}
 			if(ed_fulledit(ep->ed)==-1)
 				beep();
 			else
@@ -1688,4 +1706,19 @@ static int _isword(register int c)
 }
 #endif /* SHOPT_MULTIBYTE */
 
+static char allwhitespace(register Emacs_t *ep, register genchar *out)
+{
+	int x;
+	ep->mark = cur;
+	for(x=0; x < cur; x++)
+	{
+#if SHOPT_MULTIBYTE
+		if(!iswspace((wchar_t)out[x]))
+#else
+		if(!isspace(out[x]))
+#endif /* SHOPT_MULTIBYTE */
+			return(0);
+	}
+	return(1);
+}
 #endif /* SHOPT_ESH */

src/cmd/ksh93/edit/vi.c
@@ -52,6 +52,7 @@
 
 #if SHOPT_MULTIBYTE
 #   include	"lexstates.h"
+#   include	<wctype.h>
 #   define gencpy(a,b)	ed_gencpy(a,b)
 #   define genncpy(a,b,n)	ed_genncpy(a,b,n)
 #   define genlen(str)	ed_genlen(str)
@@ -174,6 +175,7 @@ typedef struct _vi_
 
 static const char paren_chars[] = "([{)]}";   /* for % command */
 
+static char	allwhitespace(Vi_t*);
 static void	cursor(Vi_t*, int);
 static void	del_line(Vi_t*,int);
 static int	getcount(Vi_t*,int);
@@ -704,6 +706,7 @@ static int cntlmode(Vi_t *vp)
 {
 	register int c;
 	register int i;
+	register int uparrow;
 	genchar tmp_u_space[MAXLINE];	/* temporary u_space */
 	genchar *real_u_space;		/* points to real u_space */
 	int tmp_u_column = INVALID;	/* temporary u_column */
@@ -859,6 +862,7 @@ static int cntlmode(Vi_t *vp)
 			}
 			save_v(vp);
 			cur_virt = INVALID;
+			uparrow = 0;
 			goto newhist;
 
 		case 'k':		/** get previous command **/
@@ -881,6 +885,7 @@ static int cntlmode(Vi_t *vp)
 			}
 			save_v(vp);
 			cur_virt = INVALID;
+			uparrow = 1;
 		newhist:
 			if(curhline!=histmax || cur_virt==INVALID)
 				hist_copy((char*)virtual, MAXLINE, curhline,-1);
@@ -896,6 +901,13 @@ static int cntlmode(Vi_t *vp)
 #endif /* SHOPT_MULTIBYTE */
 			if((last_virt=genlen(virtual)-1) >= 0  && cur_virt == INVALID)
 				cur_virt = 0;
+			if(allwhitespace(vp))
+			{
+				if(uparrow && curhline != histmin)
+					ed_ungetchar(vp->ed,'k');
+				if(!uparrow && curhline != histmax)
+					ed_ungetchar(vp->ed,'j');
+			}
 			break;
 
 
@@ -917,7 +929,16 @@ static int cntlmode(Vi_t *vp)
 
 		case 'v':
 			if(vp->repeat_set==0)
+			{
+				if(allwhitespace(vp) || cur_virt == INVALID)
+				{
+					cur_virt = 0;
+					last_virt = cur_virt;
+					refresh(vp,TRANSLATE);
+					virtual[last_virt++] = '\n';
+				}
 				goto vcommand;
+			}
 			/* FALLTHROUGH */
 
 		case 'G':		/** goto command repeat **/
@@ -1485,17 +1506,7 @@ static void getline(register Vi_t* vp,register int mode)
 
 		case '\t':		/** command completion **/
 		{
-			char allempty = 1;
-			int x;
-			for(x=0; x <= cur_virt; x++)
-			{
-				if(!isspace(virtual[x]))
-				{
-					allempty = 0;
-					break;
-				}
-			}
-			if(allempty)
+			if(allwhitespace(vp))
 			{
 				ed_ringbell();
 				break;
@@ -2533,17 +2544,7 @@ addin:
 	case '\\':		/** do file name completion in place **/
 	case '=':		/** list file name expansions **/
 	{
-		char allempty = 1;
-		int x;
-		for(x=0; x <= cur_virt; x++)
-		{
-			if(!isspace(virtual[x]))
-			{
-				allempty = 0;
-				break;
-			}
-		}
-		if(cur_virt == INVALID || allempty)
+		if(cur_virt == INVALID || allwhitespace(vp))
 			return(BAD);
 		/* FALLTHROUGH */
 		save_v(vp);
@@ -2883,6 +2884,24 @@ yankeol:
 	return((v&~STRIP)==0 && isspace(v));
     }
 #endif	/* SHOPT_MULTIBYTE */
+
+/*
+ * determine if a line is entirely blank
+ */
+static char allwhitespace(register Vi_t *vp)
+{
+	int x;
+	for(x=0; x <= cur_virt; x++)
+	{
+#if SHOPT_MULTIBYTE
+		if(!iswspace((wchar_t)virtual[x]))
+#else
+		if(!isspace(virtual[x]))
+#endif /* SHOPT_MULTIBYTE */
+			return(0);
+	}
+	return(1);
+}
 
 /*
  * get a character, after ^V processing

commit 0b08207a86bca43d689f66b2eb9f65621e0ebfe2

Fix infinite loop in emacs mode when HISTFILE is empty
This commit fixes a regression introduced in 5eeeed06. Pressing the up
arrow in emacs mode when the history file is empty or otherwise cannot
be accessed causes an infinite loop that repeatedly calls beep().

Reproducer (using /dev/null as HISTFILE also reproduces the bug):
echo '' > /tmp/a; ENV=/dev/null HISTFILE=/tmp/a ksh -o emacs
<Press Up Arrow>

The loop happens because when blanks lines in the history file are
skipped at L700, ed_ungetchar loops back to L635, beeps at L650, and
since ksh is compiled with ESH_NFIRST enabled by default a goto at
L655 jumps back to the code the initial ed_ungetchar is located.
Since the variable c is still ^P, this loop continues into perpetuity.
(Also of note is that if the history is not empty, but the first command
is a blank space, ksh will loop once that space is reached while using
the up arrow.)

src/cmd/ksh93/edit/emacs.c:
- Rather than use a goto that loops repeatedly, remove the ifndef
  that prevented use of the continue statement. Once the beep occurs
  we evidently can't go back any further in the command history,
  so bail out.

src/cmd/ksh93/edit/emacs.c
@@ -648,9 +648,7 @@ update:
 			{
 				hline = hismin+1;
 				beep();
-#ifndef ESH_NFIRST
 				continue;
-#endif
 			}
 			goto common;
 