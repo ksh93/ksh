01aa363540a632e21baebf01e69d543e45f9c7f4
Martijn Dekker <martijn@inlv.org>
2024-03-07 01:17:10 +0000

    Simplify .paths generation; tweaks
    src/cmd/INIT/Mamfile:
    - Move .paths from bin into dyn/bin.
    - Drastically simplify -- just cat the contents that the old code
      generated. I have no idea why that was so complicated.
    
    bin/package:
    - Remove redunant .paths generation code.
    
    src/cmd/INIT/dylink.sh:
    - Disable dynamic libraries if AST_NO_DYLIB is exported.
    - Disable dynamic libraries on untested systems unless
      AST_DYLIB_TEST is exported.
    - Tweaks.

--- src/cmd/INIT/dylink.sh
+++ src/cmd/INIT/dylink.sh
@@ -24,7 +24,7 @@ set -o noglob	# avoid interference from pathname expansion
 
 note()
 {
-	printf "dylink: %s\\n" "$@" >&2
+	printf "$0: %s\\n" "$@" >&2
 }
 
 err_out()
@@ -35,23 +35,23 @@ err_out()
 
 do_link()
 {
-	target=${dest_dir}/$1
+	target=$dest_dir/$1
 	shift
 	test -e "$target" && rm -f "$target"
 	(
 		PS4="[dylink] $PS4"
 		set -o xtrace
 		# why pass CCFLAGS here? because it *might* have a flag like -m64 that the linker also needs
-		${CC:-cc} ${CCFLAGS} ${LDFLAGS} -o "$target" "$@"
+		${CC:-cc} $CCFLAGS $LDFLAGS -o "$target" "$@"
 	) || exit
 }
 
 # Basic sanity check.
-case ${AST_DYLIB_VERSION:+A}${HOSTTYPE:+H} in
+case ${AST_DYLIB_VERSION:+A}${HOSTTYPE:+H}${AST_NO_DYLIB+n} in
 AH)
 	;;
 *)
-	echo "dylink: building dynamic libraries was not enabled; skipping" >&2
+	echo "$0: building dynamic libraries was not enabled; skipping" >&2
 	exit 0  # continue build
 	;;
 esac
@@ -78,45 +78,56 @@ esac
 case ${module_name:+m}${suffix:+s} in
 '')	;;
 ms)	case $suffix in
-	*${AST_DYLIB_VERSION}*) ;;
-	*)	err_out "suffix does not contain \${AST_DYLIB_VERSION} (${AST_DYLIB_VERSION})" ;;
+	*$AST_DYLIB_VERSION*) ;;
+	*)	err_out "suffix does not contain \$AST_DYLIB_VERSION ($AST_DYLIB_VERSION)" ;;
 	esac ;;
 *)	err_out "-m requires -s and vice versa" ;;
 esac
 
 case $HOSTTYPE in
+darwin.* | freebsd* | linux.* | openbsd.* | sol* )
+	# supported
+	;;
 cygwin.*)
-	note "Dynamic libraries are not supported on $HOSTTYPE."
+	note "Dynamic libraries are not supported on Cygwin."
 	exit 0 ;;  # continue build
+*)
+	note "The system $HOSTTYPE is currently untested for dynamic libraries" \
+		"so dynamic libraries are disabled by default. To test them," \
+		"export AST_DYLIB_TEST to try to build a dynamically linked ksh."
+	case ${AST_DYLIB_TEST:+y} in
+	y)	;;
+	* )	exit 0 ;; # continue build
+	esac ;;
 esac
 
 # Set destination directory.
-dest_dir=${INSTALLROOT}/dyn
-mkdir -p "${dest_dir}/bin" "${dest_dir}/lib" || err_out "could not mkdir"
+dest_dir=$INSTALLROOT/dyn
+mkdir -p "$dest_dir/bin" "$dest_dir/lib" || err_out "could not mkdir"
 
 # Do the dynamic linking.
 case ${exec_file} in
 '')	# ... figure out library file name(s) and internal name for linking purposes
-	lib_file=lib${module_name}$suffix
-	lib_linkname=$(echo "$lib_file" | sed "s/\.${AST_DYLIB_VERSION}/.${AST_DYLIB_VERSION%%.*}/")
-	sym_links="${lib_linkname} $(echo "$lib_file" | sed "s/\.${AST_DYLIB_VERSION}//")"
+	lib_file=lib$module_name$suffix
+	lib_linkname=$(echo "$lib_file" | sed "s/\.$AST_DYLIB_VERSION/.${AST_DYLIB_VERSION%%.*}/")
+	sym_links="$lib_linkname $(echo "$lib_file" | sed "s/\.$AST_DYLIB_VERSION//")"
 	# ... execute linker command
 	case $HOSTTYPE in
 	darwin*)
-		do_link "lib/${lib_file}" -dynamiclib \
-			-Wl,-dylib_install_name -Wl,"${lib_linkname}" \
-			"$@" -L"${dest_dir}/lib" ${l_flags}
+		do_link "lib/$lib_file" -dynamiclib \
+			-Wl,-dylib_install_name -Wl,"$lib_linkname" \
+			"$@" -L"$dest_dir/lib" $l_flags
 		;;
 	*)
-		do_link "lib/$lib_file" -shared -Wl,-soname -Wl,"${lib_linkname}" \
-			"$@" -L"${dest_dir}/lib" ${l_flags}
+		do_link "lib/$lib_file" -shared -Wl,-soname -Wl,"$lib_linkname" \
+			"$@" -L"$dest_dir/lib" $l_flags
 		;;
 	esac
-	for f in ${sym_links}
-	do	ln -sf "${lib_file}" "${dest_dir}/lib/$f"
+	for f in $sym_links
+	do	ln -sf "$lib_file" "$dest_dir/lib/$f"
 	done
 	;;
 *)	# Link an executable.
-	do_link "bin/${exec_file}" "$@" -L"${dest_dir}/lib" ${l_flags}
+	do_link "bin/$exec_file" "$@" -L"$dest_dir/lib" $l_flags
 	;;
 esac
