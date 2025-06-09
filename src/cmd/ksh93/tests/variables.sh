ksh93/ksh

commit 4a8072e826dbc848d20c0af6b540a1e50ed776a8

Fix ${!foo@} and ${!foo*} to include 'foo' itself in search
These expansions are supposed to yield all variable names beginning
with the indicated prefix. This should include the variable name
that is identical to the prefix (as 'prefix' begins with 'prefix').

This bugfix is backported from the abandoned ksh 93v- beta, so AT&T
intended this change. It also makes ksh work like bash in this.

src/cmd/ksh93/sh/macro.c: varsub(): M_NAMESCAN:
- Check if the prefix itself exists. If so, start with that.

src/cmd/ksh93/tests/variables.sh:
- Add tests for these expansions.

src/cmd/ksh93/sh.1:
- Fix the incomplete documentation of these expansions.

src/cmd/ksh93/COMPATIBILITY:
- Note the change as it's potentially incompatible in corner cases.

Resolves: https://github.com/ksh93/ksh/issues/183

src/cmd/ksh93/sh/macro.c
@@ -1517,10 +1517,15 @@ retry1:
 			}
 			else
 			{
+				/* M_NAMESCAN: ${!prefix@} or ${!prefix*}. These work like $@, $*. */
 				dolmax = strlen(id);
 				dolg = -1;
 				nextname(mp,id,0);
-				v = nextname(mp,id,dolmax);
+				/* Check if the prefix (id) itself exists. If so, start with that. */
+				if(nv_open(id,mp->shp->var_tree,NV_NOREF|NV_NOADD|NV_VARNAME|NV_NOFAIL))
+					v = id;
+				else
+					v = nextname(mp,id,dolmax);
 			}
 		}
 		else if(type==M_SUBNAME)

src/cmd/ksh93/tests/variables.sh
@@ -1248,4 +1248,15 @@ exp=42.0000000000
 [[ $a == "$exp" ]] || err_exit "\${a:=b}: a was not assigned the correct float value (expected '$exp', got '$a')"
 
 # ======
+# ${!FOO@} and ${!FOO*} expansions did not include FOO itself
+# https://github.com/ksh93/ksh/issues/183
+unset foo "${!foo@}"
+exp='foo foobar fool'
+got=$(IFS=/; foo=bar foobar=fbar fool=pity; print -r -- "${!foo@}")
+[[ $got == "$exp" ]] || err_exit "\${!foo@}: expected $(printf %q "$exp"), got $(printf %q "$got")"
+exp='foo/foobar/fool'
+got=$(IFS=/; foo=bar foobar=fbar fool=pity; print -r -- "${!foo*}")
+[[ $got == "$exp" ]] || err_exit "\${!foo*}: expected $(printf %q "$exp"), got $(printf %q "$got")"
+
+# ======
 exit $((Errors<125?Errors:125))

src/cmd/ksh93/COMPATIBILITY
@@ -96,6 +96,9 @@ For more details, see the NEWS file and for complete details, see the git log.
 	match a normal (non-**) glob pattern. For example, if '/lnk' is a
 	symlink to a directory, '/lnk/**' and '/l?k/**' now work as expected.
 
+18.	The variable name search expansions ${!prefix@} and ${!prefix*} now
+	also include the variable 'prefix' itself in the possible results.
+
 ____________________________________________________________________________
 
 		KSH-93 VS. KSH-88

src/cmd/ksh93/include/version.h
@@ -20,7 +20,7 @@
 
 #define SH_RELEASE_FORK	"93u+m"		/* only change if you develop a new ksh93 fork */
 #define SH_RELEASE_SVER	"1.0.0-alpha"	/* semantic version number: https://semver.org */
-#define SH_RELEASE_DATE	"2021-03-07"	/* must be in this format for $((.sh.version)) */
+#define SH_RELEASE_DATE	"2021-03-09"	/* must be in this format for $((.sh.version)) */
 #define SH_RELEASE_CPYR	"(c) 2020-2021 Contributors to ksh " SH_RELEASE_FORK
 
 /* Scripts sometimes field-split ${.sh.version}, so don't change amount of whitespace. */

src/cmd/ksh93/sh.1
@@ -1274,9 +1274,14 @@ and
 inclusive using the same quoting rules as
 .BR @ .
 .TP
+.PD 0
+\f3${!\fP\f2prefix\^\fP\f3@}\fP
+.TP
 \f3${!\fP\f2prefix\^\fP\f3*}\fP
-Expands to the names of the variables whose names begin with
+These both expand to the names of the variables whose names begin with
 .IR prefix .
+The expansions otherwise work like \f3$@\fP and \f3$*\fP, respectively
+(see under \f2Quoting\fP below).
 .TP
 \f3${\fP\f2parameter\^\fP\f3:\-\fP\f2word\^\fP\f3}\fP
 If

commit dce055852ba44ddee5d012bfd4faa45ecbc1ddb2

bug-875: Phi:
Fix regression introduced in Commit 4a8072e

src/cmd/ksh93/tests/variables.sh:
- In varsub(), add a local have_dot=0, during id scan record if
  we see a dot if so have_dot=1. During M_NAMESCAN loop avoid inserting
  the id itself if it have_dot.

src/cmd/ksh93/sh/macro.c
@@ -1166,6 +1166,7 @@ static int varsub(Mac_t *mp)
 	char		*idx = 0;
 	int		var=1,addsub=0,oldpat=mp->pattern,idnum=0,flag=0,d;
 	Stk_t		*stkp = sh.stk;
+	char		have_dot=0; /* bug-875: Phi: */
 	mp->wasexpan = 1;
 retry1:
 	idbuff[0] = 0;
@@ -1292,7 +1293,11 @@ retry1:
 			do
 			{
 				if(LEN==1)
+				{ 
+				  if(c=='.')
+					have_dot=1;
 					sfputc(stkp,c);
+				}
 				else
 					sfwrite(stkp,fcseek(0)-LEN,LEN);
 			}
@@ -1609,7 +1614,8 @@ retry1:
 				dolg = -1;
 				nextname(mp,id,0);
 				/* Check if the prefix (id) itself exists. If so, start with that. */
-				if(nv_open(id,sh.var_tree,NV_NOREF|NV_NOADD|NV_VARNAME|NV_NOFAIL))
+				/* bug-875: Phi: Don't do that for namespaced var*/
+				if( !have_dot && nv_open(id,sh.var_tree,NV_NOREF|NV_NOADD|NV_VARNAME|NV_NOFAIL))
 					v = id;
 				else
 					v = nextname(mp,id,dolmax);

src/cmd/ksh93/tests/variables.sh
@@ -1770,4 +1770,11 @@ got=$(./issue861.sh 2>&1)
 unset i
 
 # ======
+# https://github.com/ksh93/ksh/issues/861
+a=1 a.a=2 a.b=3 a.c=4 a.aa=5 a.ac=6
+got=$(echo ${!a.a*})
+exp="a.a a.aa a.ac"
+[[ $got == "$exp" ]] || err_exit "\${!a.*} not workig. expected '$exp' got '$got'"
+
+# ======
 exit $((Errors<125?Errors:125))