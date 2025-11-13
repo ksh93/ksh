ksh93/ksh

commit 1a9d8adaeb4415a0239b463eab73fe51239a26eb

Fix printf %(dateformat)T with relative date spec (#641)
Relative date specs such as '9 weeks ago' are completely broken,
often giving the wrong date even with the 'exact' keyword added.
Only trivial cases work correctly.

Note also that handling this date/time thing is pretty complicated,
even gnudate(1) (i.e coreutils::parse_time()) don't handle it
correctly.

Yet for this fix, I try to get as close as gdate(1) as possible
when that makes sense.

Sometime ksh (libast) printf %T implements things not supported by
gdate(1), for instance 'exact' is not implemented by gdate(1). Then
the implementation of a feature (like 'exact') can be inconsistent:
sometimes it is implemented, sometimes not. I decided not to fix
theses inconsistencies to avoid potential regressions, i.e., one
could use the faulty path on purpose and would be broken if fixed.

Some examples:
date    +'%Y-%m-%d %H:%M:%S'  --date="  now +1 month" => 2022-11-24 06:28:12
printf "%(%Y-%m-%d %H:%M:%S)T\n" "      now +1 month" => 2022-11-01 00:00:00
printf "%(%Y-%m-%d %H:%M:%S)T\n" "exact now +1 month" => 2022-11-24 06:28:12
                                  ^^^^^
As we see here, ksh(1) diverges from GNU date(1), but with 'exact',
ksh behaves like it.

BUT

F='%Y-%m-%d %H:%M:%S'
date +"$F" --date="      2020-2-3 12:34:56 +1 month" => 2020-03-03 11:34:56
printf "%($F)T\n" "      2020-2-3 12:34:56 +1 month" => 2020-03-03 12:34:56
printf "%($F)T\n" "exact 2020-2-3 12:34:56 +1 month" => 2020-03-03 12:34:56
                   ^^^^^
When 'exact' is not present here we should have zero'ed HH:MM:SS,
this is not fixed here on purpose.

FIXES: src/lib/libast/tm/tmfix.c:
=================================
The former code was working for a 1 year month roll over, but fail
in multi-year month roll over.

date +"$F" --date=now               => 2022-10-24 07:32:58
date +"$F" --date="      -20 month" => 2021-02-24 07:32:58
printf "%($F)T\n" "exact -20 month" => 2021-02-24 07:32:58

src/lib/libast/tm/tmxdate.c
@@ -15,6 +15,7 @@
 *                     Phong Vo <phongvo@gmail.com>                     *
 *                  Martijn Dekker <martijn@inlv.org>                   *
 *            Johnothan King <johnothanking@protonmail.com>             *
+*                      Phi <phi.debian@gmail.com>                      *
 *                                                                      *
 ***********************************************************************/
 /*
@@ -228,10 +229,20 @@ tmxdate(const char* s, char** e, Time_t now)
 		while (isspace(*s))
 			s++;
 		message((-1, "AHA#%d state=" FFMT " set=" FFMT " '%s'", __LINE__, FLAGS(state), FLAGS(set), s));
+
+		f = 1;			/* Default factor */
+		if (state & LAST)
+			f = -f;		/* Invert on 'last'/'ago', etc...*/
+
 		for (;;)
 		{
 			if (*s == '.' || *s == '-' || *s == '+')
 			{
+				if (*s == '-')
+					f = -1;
+				if (*s == '+')
+					f = 1;
+
 				if (((set|state) & (MONTH|HOUR|MINUTE|ZONE)) == (MONTH|HOUR|MINUTE) && (i = tmgoff(s, &t, TM_LOCALZONE)) != TM_LOCALZONE)
 				{
 					zone = i;
@@ -239,8 +250,6 @@ tmxdate(const char* s, char** e, Time_t now)
 					if (!*(s = t))
 						break;
 				}
-				else if (*s == '+')
-					break;
 			}
 			else if (!skip[*s])
 				break;
@@ -499,21 +508,7 @@ tmxdate(const char* s, char** e, Time_t now)
 			} while (c);
 			continue;
 		}
-		f = -1;
-		if (*s == '+')
-		{
-			while (isspace(*++s) || *s == '_');
-			n = strtol(s, &t, 0);
-			if (w = t - s)
-			{
-				for (s = t; skip[*s]; s++);
-				state |= (f = n) ? NEXT : THIS;
-				set &= ~(EXACT|LAST|NEXT|THIS);
-				set |= state & (EXACT|LAST|NEXT|THIS);
-			}
-			else
-				s = last;
-		}
+
 		if (!(state & CRON))
 		{
 			/*
@@ -703,7 +698,7 @@ tmxdate(const char* s, char** e, Time_t now)
 				now = n;
 				goto sns;
 			}
-			if ((*t == 'T' || *t == 't') && ((set|state) & (YEAR|MONTH|DAY)) == (YEAR|MONTH) && isdigit(*(t + 1)))
+			if ((*t == 'T' || *t == 't') && ((set|state) & (YEAR|MONTH)) == (YEAR|MONTH) && isdigit(*(t + 1)))
 				t++;
 			u = t + (*t == '-');
 			message((-1, "AHA#%d n=%d w=%d u='%c' f=%d t=\"%s\"", __LINE__, n, w, *u, f, t));
@@ -734,6 +729,8 @@ tmxdate(const char* s, char** e, Time_t now)
 				if (k == 7)
 					k = 0;
 				tm->tm_year = m;
+				if (!(state & LAST))	/* use 'exact' to get HHMMSS */
+					tm->tm_hour = tm->tm_min = tm->tm_sec = tm->tm_nsec = 0;
 				tmweek(tm, 2, n, k);
 				set |= YEAR|MONTH|DAY;
 				s = t;
@@ -811,8 +808,8 @@ tmxdate(const char* s, char** e, Time_t now)
 			{
 				message((-1, "AHA#%d n=%d", __LINE__, n));
  ordinal:
-				if (n)
-					n--;
+				if (n && k != TM_PARTS)
+					n--;	/* Not for TM_PARTS on par with gdate(1) */
 				message((-1, "AHA#%d n=%d", __LINE__, n));
 				state |= ((f = n) ? NEXT : THIS)|ORDINAL;
 				set &= ~(EXACT|LAST|NEXT|THIS);
@@ -1038,7 +1035,7 @@ tmxdate(const char* s, char** e, Time_t now)
 								tm->tm_hour = i += 12;
 							break;
 						}
-						if (f >= 0 || (state & (LAST|NEXT)))
+						if (state & (LAST|NEXT))
 						{
 							message((-1, "AHA#%d f=%d i=%d j=%d k=%d l=%d", __LINE__, f, i, j, k, l));
 							state &= ~HOLD;
@@ -1085,12 +1082,9 @@ tmxdate(const char* s, char** e, Time_t now)
 		for (;;)
 		{
 			message((-1, "AHA#%d s=\"%s\"", __LINE__, s));
+			/* +/- handled on top of loop */
 			if (*s == '-' || *s == '+')
-			{
-				if (((set|state) & (MONTH|DAY|HOUR|MINUTE)) == (MONTH|DAY|HOUR|MINUTE) || *s == '+' && (!isdigit(s[1]) || !isdigit(s[2]) || s[3] != ':' && (s[3] != '.' || ((set|state) & (YEAR|MONTH)) != (YEAR|MONTH))))
-					break;
-				s++;
-			}
+				break;
 			else if (skip[*s])
 				s++;
 			else
@@ -1265,6 +1259,13 @@ tmxdate(const char* s, char** e, Time_t now)
 					case TM_ORDINALS:
 						n = j - TM_ORDINALS + 1;
 						message((-1, "AHA#%d n=%d", __LINE__, n));
+						/* look ahead for TM_PARTS, in k used in ordinal: */
+						u = t;
+						while (skip[*u])
+							u++;
+						k = tmlex(u, &u,
+							tm_info.format, TM_NFORM,
+							tm_info.format + TM_SUFFIXES, TM_PARTS - TM_SUFFIXES);
 						goto ordinal;
 					case TM_MERIDIAN:
 						if (f >= 0)
@@ -1302,31 +1303,67 @@ tmxdate(const char* s, char** e, Time_t now)
 					case TM_PARTS:
 					case TM_HOURS:
 						state |= set & (EXACT|LAST|NEXT|THIS);
-						if (!(state & (LAST|NEXT|THIS)))
-							for (;;)
+						/*
+						 * disambiguate english "second"
+						 */
+						if (n < 0 && j == TM_PARTS && !(state & (LAST | NEXT | THIS)))
+						{
+							/* look ahead for TM_PARTS, k used in ordinal: */
+							u = t;
+							while (skip[*u])
+								u++;
+							k = tmlex(u, &u,
+								tm_info.format, TM_NFORM,
+								tm_info.format + TM_SUFFIXES, TM_PARTS - TM_SUFFIXES);
+							if (k > 0)
 							{
-								while (skip[*s])
-									s++;
-								if ((k = tmlex(s, &t, tm_info.format + TM_LAST, TM_NOISE - TM_LAST, NULL, 0)) >= 0)
+								n = 2;
+								goto ordinal;
+							}
+							n = 1;
+						}
+
+						for (;;)
+						{
+							while (skip[*s])
+								s++;
+							if ((k = tmlex(s, &t, tm_info.format + TM_LAST, TM_NOISE - TM_LAST, NULL, 0)) >= 0)
+							{
+								s = t;
+								if (k <= 2)
 								{
-									s = t;
-									if (k <= 2)
+									/* THIS takes precedence */
+									if (!(state & THIS))
+									{
 										state |= LAST;
-									else if (k <= 5)
-										state |= THIS;
-									else if (k <= 8)
-										state |= NEXT;
-									else
-										state |= EXACT;
+										f = -f;
+									}
+								}
+								else if (k <= 5)
+									state |= THIS;
+								else if (k <= 8)
+									state |= NEXT;
+								else
+									state |= EXACT;
+							}
+							else
+							{
+								if(n>=0)
+								{
+									state |= NEXT;
 								}
 								else
 								{
-									state |= (n > 0) ? NEXT : THIS;
-									break;
+									state |= THIS;
+									n= state & LAST ? 1 : 0;
+									if (state & (LAST | NEXT))
+										n = 1; /* f is -1(LAST|AGO) or +1(THIS|NEXT) */
 								}
-								set &= ~(EXACT|LAST|NEXT|THIS);
-								set |= state & (EXACT|LAST|NEXT|THIS);
+								break;
 							}
+							set &= ~(EXACT|LAST|NEXT|THIS);
+							set |= state & (EXACT|LAST|NEXT|THIS);
+						}
 						/* FALLTHROUGH */
 					case TM_DAYS:
 						message((-1, "AHA#%d n=%d j=%d f=%d state=" FFMT, __LINE__, n, j, f, FLAGS(state)));
@@ -1367,11 +1404,9 @@ tmxdate(const char* s, char** e, Time_t now)
 								state &= ~(THIS|NEXT);
 						}
 						message((-1, "AHA#%d disambiguate LAST k=%d", __LINE__, k));
-						if (state & LAST)
-							n = -n;
-						else if (!(state & NEXT))
-							n--;
-						m = (f > 0) ? f * n : n;
+
+						m = f * n;  /* f is -1, 1 n is >=0 (except for ordinals for historical reasons) */
+
 						message((-1, "AHA#%d f=%d n=%d i=%d j=%d k=%d l=%d m=%d state=" FFMT, __LINE__, f, n, i, j, k, l, m, FLAGS(state)));
 						switch (j)
 						{
@@ -1416,7 +1451,7 @@ tmxdate(const char* s, char** e, Time_t now)
 							goto clear_hour;
 						case TM_PARTS+4:
 							tm = tmxtm(tm, tmxtime(tm, zone), tm->tm_zone);
-							tm->tm_mday += 7 * m - tm->tm_wday + 1;
+							tm->tm_hour += m * 7 * 24;
 							set |= DAY;
 							goto clear_hour;
 						case TM_PARTS+5:

src/cmd/ksh93/include/version.h
@@ -18,7 +18,7 @@
 
 #define SH_RELEASE_FORK	"93u+m"		/* only change if you develop a new ksh93 fork */
 #define SH_RELEASE_SVER	"1.1.0-alpha"	/* semantic version number: https://semver.org */
-#define SH_RELEASE_DATE	"2023-05-17"	/* must be in this format for $((.sh.version)) */
+#define SH_RELEASE_DATE	"2023-05-18"	/* must be in this format for $((.sh.version)) */
 #define SH_RELEASE_CPYR	"(c) 2020-2023 Contributors to ksh " SH_RELEASE_FORK
 
 /* Scripts sometimes field-split ${.sh.version}, so don't change amount of whitespace. */

src/cmd/ksh93/tests/printf.sh
@@ -15,17 +15,22 @@
 #                                                                      #
 ########################################################################
 
-PS1='$ '
 . "${SHTESTS_COMMON:-${0%/*}/_common}"
 
-# ======
-# Test for https://github.com/ksh93/ksh/issues/324
+if	let '.sh.version < 20211118'
+then	err_exit 'ksh too old for "printf -v"; tests would fail massively'
+	exit 1
+fi
 
-function T
-{
-	typeset g
-	printf -v g "$2" 1 2 3 4 5 6 7 8 9 0
-	[[ $g == "$3" ]] || \err_exit "$1" "printf '$2': expected $(printf %q "$3"), got $(printf %q "$g")"
+alias T='do_test "$LINENO"'
+
+# ======
+# Tests for mixing and matching % and %x$
+# https://github.com/ksh93/ksh/issues/324
+
+function do_test
+{	printf -v got "$2" 1 2 3 4 5 6 7 8 9 0
+	[[ $got == "$3" ]] || \err_exit "$1" "printf '$2': expected $(printf %q "$3"), got $(printf %q "$got")"
 }
 
 # This part was generated once, and can now be augmented with more test
@@ -37,7 +42,7 @@ x='1 2
 9 0
 '
 f='%s %s\n'
-T $LINENO "$f" "$x"
+T "$f" "$x"
 
 x='1 1
 2 2
@@ -51,7 +56,7 @@ x='1 1
 0 0
 '
 f='%s %1$s\n'
-T $LINENO "$f" "$x"
+T "$f" "$x"
 
 x='1 1
 2 2
@@ -65,7 +70,7 @@ x='1 1
 0 0
 '
 f='%1$s %s\n'
-T $LINENO "$f" "$x"
+T "$f" "$x"
 
 x='1 1
 2 2
@@ -79,7 +84,7 @@ x='1 1
 0 0
 '
 f='%1$s %1$s\n'
-T $LINENO "$f" "$x"
+T "$f" "$x"
 
 x='1 1 2 2
 3 3 4 4
@@ -88,7 +93,7 @@ x='1 1 2 2
 9 9 0 0
 '
 f='%1$s %s %s %2$s\n'
-T $LINENO "$f" "$x"
+T "$f" "$x"
 
 x='1 1 2 3
 4 4 5 6
@@ -96,7 +101,7 @@ x='1 1 2 3
 0 0  
 '
 f='%s %1$s %s %s\n'
-T $LINENO "$f" "$x"
+T "$f" "$x"
 
 x='1 1 2 2
 3 3 4 4
@@ -105,7 +110,7 @@ x='1 1 2 2
 9 9 0 0
 '
 f='%s %1$s %2$s %s\n'
-T $LINENO "$f" "$x"
+T "$f" "$x"
 
 x='2 1
 4 3
@@ -114,7 +119,7 @@ x='2 1
 0 9
 '
 f='%2$s %s\n'
-T $LINENO "$f" "$x"
+T "$f" "$x"
 
 x='2 1 2 1
 4 3 4 3
@@ -123,7 +128,7 @@ x='2 1 2 1
 0 9 0 9
 '
 f='%2$s %1$s %2$s %1$s\n'
-T $LINENO "$f" "$x"
+T "$f" "$x"
 
 x='2 1 1 2 1 2
 4 3 3 4 3 4
@@ -132,7 +137,7 @@ x='2 1 1 2 1 2
 0 9 9 0 9 0
 '
 f='%2$s %1$s %s %2$s %1$s %s\n'
-T $LINENO "$f" "$x"
+T "$f" "$x"
 
 x=' 1 2
    3 4
@@ -141,7 +146,7 @@ x=' 1 2
 9 0
 '
 f='%*2$s %s\n'
-T $LINENO "$f" "$x"
+T "$f" "$x"
 
 x=' 1 2
     4 5
@@ -149,7 +154,7 @@ x=' 1 2
  
 '
 f='%*2$.*3$s %s\n'
-T $LINENO "$f" "$x"
+T "$f" "$x"
 
 x='  1 2
      4 5
@@ -157,25 +162,25 @@ x='  1 2
  
 '
 f='%*3$.*2$s %s\n'
-T $LINENO "$f" "$x"
+T "$f" "$x"
 
 x='2   5
      7        0
 '
 f='%*s %*.*s\n'
-T $LINENO "$f" "$x"
+T "$f" "$x"
 
 x='2 0005
      7 000000000
 '
 f='%*d %*.*d\n'
-T $LINENO "$f" "$x"
+T "$f" "$x"
 
 x='2.000000 5.0000
 7.000000 0.000000000
 '
 f='%*f %*.*f\n'
-T $LINENO "$f" "$x"
+T "$f" "$x"
 
 x=' 3 2 1
     6 5 4
@@ -183,13 +188,391 @@ x=' 3 2 1
   0
 '
 f='%3$*2$.*1$s %2$s %1$s\n'
-T $LINENO "$f" "$x"
+T "$f" "$x"
 
 x='1 5
 6 0
 '
 f='%s %5$s\n'
-T $LINENO "$f" "$x"
+T "$f" "$x"
+
+unset x f
+
+# ======
+# Tests for printf %T with relative date spec and 'exact' keyword
+# https://github.com/ksh93/ksh/issues/182
+
+export TZ=UTC
+
+# Check printf against a string
+function do_test # 1:LINENO 2:printf-STRING 3:match-string
+{	printf -v got "%($format)T" "$2"
+	[[ $got == "$3" ]] || \err_exit "$1" "$C: printf '%($format)T' '$2':" \
+		"expected $(printf %q "$3"), got $(printf %q "$got")"
+}
+
+format='%Y-%m-%d'
+
+C='Calendar dates'
+T '2020-01-14'				'2020-01-14'
+T '2020-01-14 -14 days'			'2019-12-31'
+T '2020-01-14 -14 days ago'		'2020-01-28'
+T '2020-01-14 -1 days'			'2020-01-13'
+T '2020-01-14 -1 days ago'		'2020-01-15'
+T '2020-01-14 -0 days'			'2020-01-14'
+T '2020-01-14 -0 days ago'		'2020-01-14'
+T '2020-01-14 +0 days'			'2020-01-14'
+T '2020-01-14 +0 days ago'		'2020-01-14'
+T '2020-01-14 +1 days'			'2020-01-15'
+T '2020-01-14 +1 days ago'		'2020-01-13'
+T '2020-01-14 +14 days'			'2020-01-28'
+T '2020-01-14 +14 days ago'		'2019-12-31'
+T '2020-01-14 0 days'			'2020-01-14'
+T '2020-01-14 0 days ago'		'2020-01-14'
+T '2020-01-14 1 days'			'2020-01-15'
+T '2020-01-14 1 days ago'		'2020-01-13'
+T '2020-01-14 14 days'			'2020-01-28'
+T '2020-01-14 14 days ago'		'2019-12-31'
+
+format='%Y-%m-%d %H:%M:%S'
+
+C='Backward rollover'
+T '#1684340457'				'2023-05-17 16:20:57'
+T '#1684340457 exact 2400 min  ago'	'2023-05-16 00:20:57'
+T '#1684340457 exact   40 hour ago'	'2023-05-16 00:20:57'
+
+C='Ambigous sec'
+T '#1579042800            +120 seconds'	'2020-01-14 23:02:00'
+T '#1579042800        fri +120 seconds'	'2020-01-17 00:02:00'
+T '#1579042800 next   fri +120 seconds'	'2020-01-24 00:02:00'
+T '#1579042800 second fri +120 seconds'	'2020-01-10 00:02:00'
+T '#1579042800 second'			'2020-01-14 23:00:01'
+T '#1579042800 second second'		'2020-01-14 23:00:02'
+T '#1579042800 second second second'	'2020-01-14 23:00:03'
+T '#1579042800 second minute second'	'2020-01-14 23:01:01'
+T '#1579042800 second minute +2 second'	'2020-01-14 23:01:02'
+
+# ksh doesn't handle week dates with day number: 2020-W20-D20
+C='Week dates'
+T '2020W20'				'2020-05-11 00:00:00'
+T '2020W20 exact'			'2020-05-11 00:00:00'
+T '2020-W20'				'2020-05-11 00:00:00'
+T '2020-W20 exact'			'2020-05-11 00:00:00'
+T '2020W20  fri'			'2020-05-15 00:00:00'
+T '2020-W20 fri'			'2020-05-15 00:00:00'
+T '2020W20  next fri'			'2020-05-22 00:00:00'
+T '2020-W20 next fri'			'2020-05-22 00:00:00'
+T '2020W20  second fri'			'2020-05-08 00:00:00'
+T '2020-W20 second fri'			'2020-05-08 00:00:00'
+T '2020W20  third fri'			'2020-05-15 00:00:00'
+T '2020-W20 third fri'			'2020-05-15 00:00:00'
+
+C='Ordinal time'  # ksh only supports the extended form
+T '2020-2-3T12:34:56'			'2020-02-03 12:34:56'
+T '2020-2-3 12:34:56'			'2020-02-03 12:34:56'
+T '2020-2-3 12:34:56 next fri'		'2020-02-14 12:34:56'
+# Ordinal time with time zone (we run with TZ=UTC)
+# (ksh doesn't handle UTC offset UTC+02:30)
+T '2020-2-3T12:34:56Z'			'2020-02-03 12:34:56'
+T '2020-2-3 12:34:56Z'			'2020-02-03 12:34:56'
+
+# The following tests for times relative to the current time require GNU 'date' to compare our results to.
+if	! gd=$(	set -o noglob
+		IFS=:
+		search=$PATH:$userPATH:   # userPATH is the user's original path (saved in tests/shtests)
+		for p in $search
+		do	[[ -z $p ]] && p=.
+			for c in gnudate gdate date
+			do	if	[[ -x $p/$c && $(LC_ALL=C "$p/$c" --version 2>/dev/null) == 'date (GNU coreutils)'* ]]
+				then	print -r -- "$p/$c"
+					exit 0
+				fi
+			done
+		done
+		exit 1
+	)
+then
+	warning "GNU 'date' not available -- printf %T 'ago' tests skipped"
+else
+	# Check printf %T with 'exact' keyword against GNU 'date'
+	function do_test # 1:LINENO 2:date-STRING [3:printf-STRING]
+	{	typeset -si i
+		for ((i=0; i<2; i++))
+		{	exp=${ "$gd" +"$format" --date="$2"; }
+			printf -v got "%($format)T" "exact ${3:-$2}"
+			[[ $got == "$exp" ]] && return
+		}
+		\err_exit "$1" "check against GNU 'date': printf '%($format)T' 'exact ${3:-$2}':" \
+			"expected $(printf %q "$exp"), got $(printf %q "$got")"
+	}
+
+	# Trivial
+	T 'now'
+
+	# Abs time
+	T '@1234567890.987654321' '#1234567890.987654321'
+
+	# The ago case, inspired by the #182 reproducer. Here we do less testing
+	# than that reproducer, but span on more rollover, to trigger a bug that
+	# works on 1 rollover but not mode (modulo bugs)
+	T '-40 years'
+	T '-40 years ago'
+	T '-40 months'
+	T '-40 months ago'
+	T '-40 weeks'
+	T '-40 weeks ago'
+	T '-40 days'
+	T '-40 days ago'
+	T '-40 hours'
+	T '-40 hours ago'
+	T '-40 minutes'
+	T '-40 minutes ago'
+	T '-40 seconds'
+	T '-40 seconds ago'
+	T '-20 years'
+	T '-20 years ago'
+	T '-20 months'
+	T '-20 months ago'
+	T '-20 weeks'
+	T '-20 weeks ago'
+	T '-20 days'
+	T '-20 days ago'
+	T '-20 hours'
+	T '-20 hours ago'
+	T '-20 minutes'
+	T '-20 minutes ago'
+	T '-20 seconds'
+	T '-20 seconds ago'
+	T '-10 years'
+	T '-10 years ago'
+	T '-10 months'
+	T '-10 months ago'
+	T '-10 weeks'
+	T '-10 weeks ago'
+	T '-10 days'
+	T '-10 days ago'
+	T '-10 hours'
+	T '-10 hours ago'
+	T '-10 minutes'
+	T '-10 minutes ago'
+	T '-10 seconds'
+	T '-10 seconds ago'
+	T '-5 years'
+	T '-5 years ago'
+	T '-5 months'
+	T '-5 months ago'
+	T '-5 weeks'
+	T '-5 weeks ago'
+	T '-5 days'
+	T '-5 days ago'
+	T '-5 hours'
+	T '-5 hours ago'
+	T '-5 minutes'
+	T '-5 minutes ago'
+	T '-5 seconds'
+	T '-5 seconds ago'
+	T '0 years'
+	T '0 years ago'
+	T '0 months'
+	T '0 months ago'
+	T '0 weeks'
+	T '0 weeks ago'
+	T '0 days'
+	T '0 days ago'
+	T '0 hours'
+	T '0 hours ago'
+	T '0 minutes'
+	T '0 minutes ago'
+	T '0 seconds'
+	T '0 seconds ago'
+	T '0 years'
+	T '0 years ago'
+	T '0 months'
+	T '0 months ago'
+	T '0 weeks'
+	T '0 weeks ago'
+	T '0 days'
+	T '0 days ago'
+	T '0 hours'
+	T '0 hours ago'
+	T '0 minutes'
+	T '0 minutes ago'
+	T '0 seconds'
+	T '0 seconds ago'
+	T '0 years'
+	T '0 years ago'
+	T '0 months'
+	T '0 months ago'
+	T '0 weeks'
+	T '0 weeks ago'
+	T '0 days'
+	T '0 days ago'
+	T '0 hours'
+	T '0 hours ago'
+	T '0 minutes'
+	T '0 minutes ago'
+	T '0 seconds'
+	T '0 seconds ago'
+	T '5 years'
+	T '5 years ago'
+	T '5 months'
+	T '5 months ago'
+	T '5 weeks'
+	T '5 weeks ago'
+	T '5 days'
+	T '5 days ago'
+	T '5 hours'
+	T '5 hours ago'
+	T '5 minutes'
+	T '5 minutes ago'
+	T '5 seconds'
+	T '5 seconds ago'
+	T '5 years'
+	T '5 years ago'
+	T '5 months'
+	T '5 months ago'
+	T '5 weeks'
+	T '5 weeks ago'
+	T '5 days'
+	T '5 days ago'
+	T '5 hours'
+	T '5 hours ago'
+	T '5 minutes'
+	T '5 minutes ago'
+	T '5 seconds'
+	T '5 seconds ago'
+	T '10 years'
+	T '10 years ago'
+	T '10 months'
+	T '10 months ago'
+	T '10 weeks'
+	T '10 weeks ago'
+	T '10 days'
+	T '10 days ago'
+	T '10 hours'
+	T '10 hours ago'
+	T '10 minutes'
+	T '10 minutes ago'
+	T '10 seconds'
+	T '10 seconds ago'
+	T '10 years'
+	T '10 years ago'
+	T '10 months'
+	T '10 months ago'
+	T '10 weeks'
+	T '10 weeks ago'
+	T '10 days'
+	T '10 days ago'
+	T '10 hours'
+	T '10 hours ago'
+	T '10 minutes'
+	T '10 minutes ago'
+	T '10 seconds'
+	T '10 seconds ago'
+	T '20 years'
+	T '20 years ago'
+	T '20 months'
+	T '20 months ago'
+	T '20 weeks'
+	T '20 weeks ago'
+	T '20 days'
+	T '20 days ago'
+	T '20 hours'
+	T '20 hours ago'
+	T '20 minutes'
+	T '20 minutes ago'
+	T '20 seconds'
+	T '20 seconds ago'
+	T '20 years'
+	T '20 years ago'
+	T '20 months'
+	T '20 months ago'
+	T '20 weeks'
+	T '20 weeks ago'
+	T '20 days'
+	T '20 days ago'
+	T '20 hours'
+	T '20 hours ago'
+	T '20 minutes'
+	T '20 minutes ago'
+	T '20 seconds'
+	T '20 seconds ago'
+	T '40 years'
+	T '40 years ago'
+	T '40 months'
+	T '40 months ago'
+	T '40 weeks'
+	T '40 weeks ago'
+	T '40 days'
+	T '40 days ago'
+	T '40 hours'
+	T '40 hours ago'
+	T '40 minutes'
+	T '40 minutes ago'
+	T '40 seconds'
+	T '40 seconds ago'
+	T '40 years'
+	T '40 years ago'
+	T '40 months'
+	T '40 months ago'
+	T '40 weeks'
+	T '40 weeks ago'
+	T '40 days'
+	T '40 days ago'
+	T '40 hours'
+	T '40 hours ago'
+	T '40 minutes'
+	T '40 minutes ago'
+	T '40 seconds'
+	T '40 seconds ago'
+
+	# Various keywords: last, this, next
+	# Note that ksh doesn't support fortnight
+	T 'last year'
+	T 'last year ago'
+	T 'last month'
+	T 'last month ago'
+	T 'last week'
+	T 'last week ago'
+	T 'last day'
+	T 'last day ago'
+	T 'last hour'
+	T 'last hour ago'
+	T 'last minute'
+	T 'last minute ago'
+	T 'last second'
+	T 'last second ago'
+	T 'this year'
+	T 'this year ago'
+	T 'this month'
+	T 'this month ago'
+	T 'this week'
+	T 'this week ago'
+	T 'this day'
+	T 'this day ago'
+	T 'this hour'
+	T 'this hour ago'
+	T 'this minute'
+	T 'this minute ago'
+	T 'this second'
+	T 'this second ago'
+	T 'next year'
+	T 'next year ago'
+	T 'next month'
+	T 'next month ago'
+	T 'next week'
+	T 'next week ago'
+	T 'next day'
+	T 'next day ago'
+	T 'next hour'
+	T 'next hour ago'
+	T 'next minute'
+	T 'next minute ago'
+	T 'next second'
+	T 'next second ago'
+fi
+
+unset format gd
 
 # ======
 exit $((Errors<125?Errors:125))

src/cmd/ksh93/tests/shtests
@@ -8,7 +8,7 @@ valgrindflags='--xml=yes --log-file=/dev/null --track-origins=yes --read-var-inf
 
 USAGE=$'
 [-s8?
-@(#)$Id: shtests (ksh 93u+m) 2021-12-28 $
+@(#)$Id: shtests (ksh 93u+m) 2023-05-18 $
 ]
 [-author?David Korn <dgk@research.att.com>]
 [-author?Glenn Fowler <gsf@research.att.com>]
@@ -265,6 +265,7 @@ case $SHELL in
 */*)	SHELL=$d/$SHELL;;
 *)	SHELL=$(whence $SHELL);;
 esac
+export userPATH=$PATH  # in case test scripts need it
 PATH=$(
 	builtin getconf 2>/dev/null || PATH=/run/current-system/sw/bin:/usr/xpg7/bin:/usr/xpg6/bin:/usr/xpg4/bin:/bin:/usr/bin:$PATH
 	getconf PATH 2>/dev/null
@@ -347,7 +348,7 @@ do	[[ $i == *.sh ]] || i+='.sh'
 	t=$(	case $i in
 		glob.sh)	grep -c '^[[:blank:]]*test_[a-z]\{3,\}' $i ;;
 		leaks.sh)	grep -c ^TEST $i ;;
-		printf.sh)	grep -c '^T ' $i ;;
+		printf.sh)	grep -c '^[[:blank:]]*T ' $i ;;
 		pty.sh)		grep -c 'tst ' $i ;;
 		*)		grep -c err_exit $i ;;
 		esac	)

src/lib/libast/tm/tmfix.c
@@ -14,6 +14,7 @@
 *                  David Korn <dgk@research.att.com>                   *
 *                   Phong Vo <kpv@research.att.com>                    *
 *                  Martijn Dekker <martijn@inlv.org>                   *
+*                      Phi <phi.debian@gmail.com>                      *
 *                                                                      *
 ***********************************************************************/
 /*
@@ -95,6 +96,8 @@ tmfix(Tm_t* tm)
 	{
 		tm->tm_mday -= (23 - n) / 24;
 		tm->tm_hour = 24 - (-n) % 24;
+		if (tm->tm_hour >= 24)
+			tm->tm_hour = 0;
 	}
 	else if (n >= 24)
 	{
@@ -108,12 +111,10 @@ tmfix(Tm_t* tm)
 	}
 	else if (tm->tm_mon < 0)
 	{
-		tm->tm_year--;
-		if ((tm->tm_mon += 12) < 0)
-		{
-			tm->tm_year += tm->tm_mon / 12;
-			tm->tm_mon = (-tm->tm_mon) % 12;
-		}
+		tm->tm_year += tm->tm_mon / 12 - 1;
+		tm->tm_mon = tm->tm_mon % 12 + 12;
+		if (tm->tm_mon >= 12)
+			tm->tm_year++, tm->tm_mon = 0;
 	}
 	while (tm->tm_mday < -365)
 	{

commit 3250c5efa0dd8ec00acda336fbbc442bd962c20b

commit 1a9d8ada introduced a bug printf "%(%Y-%m-%d)T\n" "now in 10 week" produce 2025-11-13 This fix produce the expecteed 2026-01-22
src/lib/libast/tm/tmxdate.c::1456
case TM_PARTS+4:
     tm = tmxtm(tm, tmxtime(tm, zone), tm->tm_zone, 0);
     tm->tm_hour += m * 7 * 24;
     set |= DAY; <======= TYPO HERE
goto clear_hour;

Since we are updating tm_hour then set HOUR

src/lib/libast/tm/tmxdate.c
@@ -1453,7 +1453,7 @@ tmxdate(const char* s, char** e, Time_t now)
 						case TM_PARTS+4:
 							tm = tmxtm(tm, tmxtime(tm, zone), tm->tm_zone, 0);
 							tm->tm_hour += m * 7 * 24;
-							set |= DAY;
+							set |= HOUR;
 							goto clear_hour;
 						case TM_PARTS+5:
 							tm->tm_mon += m;