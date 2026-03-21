058a67ea328da8acb773ebdd042e0a38b9becc1f
Martijn Dekker <martijn@inlv.org>
2026-03-21 23:51:43 +0000

    Don't rely on NSIG (re: 254f8357)
    I should have known better than to rely on a non-standardised macro
    without at least testing *all* the systems.
    
    On FreeBSD, NSIG is defined as 32 but the maximum in-use signal
    number is 126 (for SIGRTMAX). Result: segfaults in regression test.
    
    So now we use a hardcoded size of 16 bytes = 128 bits (for up to
    128 signals) for sh.st.trapnofree. This should be safe enough.
    
    POSIX may not explicitly guarantee there won't be more than 128
    signals, but does specify that the signal number must fit into a
    1-byte exit status with the most significant bit set (signal number
    + 128), which implies a theoretical maximum signal number of 127.

--- src/cmd/ksh93/include/shell.h
+++ src/cmd/ksh93/include/shell.h
@@ -190,12 +190,6 @@ typedef struct sh_scope
 
 #if _BLD_ksh
 
-#if NSIG
-#define KSH_NSIG	(NSIG)
-#else
-#define KSH_NSIG	128		/* no UN*X system is currently known to have more than 128 signals */
-#endif
-
 /* Private interface to shell scope. The first members must match the public interface. */
 struct sh_scoped
 {
@@ -226,7 +220,7 @@ struct sh_scoped
 	char		**trapcom;	/* EXIT and signals */
 	char		**otrapcom;	/* save parent EXIT and signals for v=$(trap) */
 	struct Ufunction *real_fun;	/* current 'function name' function */
-	uint8_t		trapnofree[(KSH_NSIG+7)/8];  /* bitmask to stop b_trap() freeing trapcom entries */
+	uint8_t		trapnofree[16];	/* bitmask to stop b_trap() freeing trapcom entries */
 };
 
 struct limits
