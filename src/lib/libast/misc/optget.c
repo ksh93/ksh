ed1c344835d1d159b501506e946060088d5a6f1d
Martijn Dekker <martijn@inlv.org>
2026-03-28 03:09:35 +0000

    optget: fix out of bounds read while caching options
    The following reproducer crashes, at least under ASan:
    
       ksh -c 'LANG=C.UTF-8; print -あ-い-う- foo'
    
    This crash happens in the option caching code.
    
    src/lib/libast/misc/optget.c: optget():
    - Add mising bounds check before accessing map. The check has to be
      repeated, because c may have changed in the previous line.

--- src/lib/libast/misc/optget.c
+++ src/lib/libast/misc/optget.c
@@ -2,7 +2,7 @@
 *                                                                      *
 *               This software is part of the ast package               *
 *          Copyright (c) 1985-2012 AT&T Intellectual Property          *
-*          Copyright (c) 2020-2025 Contributors to ksh 93u+m           *
+*          Copyright (c) 2020-2026 Contributors to ksh 93u+m           *
 *                      and is licensed under the                       *
 *                 Eclipse Public License, Version 2.0                  *
 *                                                                      *
@@ -32,7 +32,7 @@
 #include <ccode.h>
 #include <ctype.h>
 
-#define OPTGET_VERSION	"optget (ksh 93u+m) 2025-05-02"
+#define OPTGET_VERSION	"optget (ksh 93u+m) 2026-03-28"
 
 #define KEEP		"*[A-Za-z][A-Za-z]*"
 #define OMIT		"*@(\\[[-+]*\\?*\\]|\\@\\(#\\)|Copyright \\(c\\)|\\$\\I\\d\\: )*"
@@ -4601,7 +4601,7 @@ optget(char** argv, const char* oopts)
 			{
 				if (c >= 0 && c < sizeof(map) && map[c] && cache->equiv[map[c]])
 					c = cache->equiv[map[c]];
-				if (k = cache->flags[map[c]])
+				if (c >= 0 && c < sizeof(map) && map[c] && (k = cache->flags[map[c]]))
 				{
 					opt_info.arg = 0;
 
