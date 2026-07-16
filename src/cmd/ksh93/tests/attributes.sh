e7eb4f682d596f0e5b857717317ad1bbd763ef25
Martijn Dekker <martijn@inlv.org>
2026-07-16 15:52:17 +0100

    ksh: nv_putval: Update size after reducing size of a -b var's value
    Derek Newhall (@dnewhall) reports that the value of a 'typeset -b'
    binary value contains garbage after emptying a pre-existing value.
    For his reproducer, see the test now added to tests/attributes.sh.
    
    Johnothan King (@JohnoKing) comments:
    > This is caused by a buffer overflow (the reproducer crashes under
    > ASan). After debugging, that overflow is seemingly caused by ksh
    > failing to update the size if the variable is smaller than it was
    > previously.
    
    src/cmd/ksh93/sh/name.c: nv_putval():
    - When the size of a -b variable is reduced, nv_putval returns
      without updating the node's nvsize field. Update it using the
      nv_setsize macro before returning. Patch by @JohnoKing.
    
    Resolves: https://github.com/ksh93/ksh/issues/892

--- src/cmd/ksh93/tests/attributes.sh
+++ src/cmd/ksh93/tests/attributes.sh
@@ -858,4 +858,43 @@ got=$(s=$'1\n2\a3\t4\v5'; typeset -L s; typeset -p s)
 	"(expected $(printf %q "$exp"); got $(printf %q "$got"))"
 
 # ======
+# Garbage left in binary variable after assigning nothing
+# Reproducer by Derek Newhall (@dnewhall)
+# https://github.com/ksh93/ksh/issues/892
+exp=$'[0]  \n[12] Zm9vYmFyCg== foobar\n\n[0]  \n[12] Zm9vYmFyCg== foobar\n\n[0]  \n[0]  \n[0]  '
+got=$(set +x; redirect 2>&1; "$SHELL" -s <<-'EOF'
+	# Define a binary variable
+	typeset -b bin1
+	# Currently empty: '[0]  '
+	printf "[%d] %s %B\n" ${#bin1} "$bin1" bin1
+	# Set to "foobar" using read: '[12] Zm9vYmFyCg== foobar'
+	read -n 1024 bin1 <<<"foobar"
+	printf "[%d] %s %B\n" ${#bin1} "$bin1" bin1
+	# Set to nothing
+	bin1=
+	# Now prints garbage: '[12] ADmRhH9iAA== 9��b'
+	printf "[%d] %s %B\n" ${#bin1} "$bin1" bin1
+	# Reset to foobar using base64
+	bin1=Zm9vYmFyCg==
+	# Works: '[12] Zm9vYmFyCg== foobar'
+	printf "[%d] %s %B\n" ${#bin1} "$bin1" bin1
+	# Set to nothing again
+	bin1=
+	# Different garbage: '12] ACeIhH9iAA== '��b~$ '
+	printf "[%d] %s %B\n" ${#bin1} "$bin1" bin1
+	# Reset using pad characters
+	bin1====
+	# Empty again: '[0]  '
+	printf "[%d] %s %B\n" ${#bin1} "$bin1" bin1
+	# Set to nothing
+	bin1=
+	# Still empty: '[0]  '
+	printf "[%d] %s %B\n" ${#bin1} "$bin1" bin1
+	EOF
+)
+[[ e=$? -eq 0 && $got == "$exp" ]] || err_exit "emptying a binary variable's value" \
+	"(expected status 0 and $(printf %q "$exp")," \
+	"got status $e$( ((e>128)) && print -n /SIG && kill -l "$e") and $(printf %q "$got"))"
+
+# ======
 exit $((Errors<125?Errors:125))
