c03ced80029fc074e4bfb783efab72db8086ad3e
Martijn Dekker <martijn@inlv.org>
2026-07-17 05:20:13 +0100

    Fix parens in e.g. ${1+a)c} (re: 5ed9ffd6, d087b031, f31e3687)
    @avih reports:
    > The following:
    >	ksh -c 'echo "${1+)}"'
    >	ksh -c 'echo "${1+a)c}"'
    >	ksh -c 'echo "${1:+a)b}"'
    > error with
    >	syntax error at line 1: `)' unexpected
    > In both default and POSIX modes (--posix).
    > All other shells I have access to think it's fine.
    > No such issue with non-positional parameter. These are OK:
    >	ksh -c 'echo "${x+)}"'
    >	ksh -c 'echo "${x+a)c}"'
    >	ksh -c 'echo "${x:+a)b}"'
    
    This had indeed been fixed for regular variables in the referenced
    commits. I've traced the remaining problem with positional
    parameters to an additional problem in the lexer.
    
    Analysis (refer to state tables in data/lexstates.c):
    
    When lexing ${1+)}, when we reach the 1 character, the active state
    table mode is ST_DOL. In this state table, digits are S_DIG and
    alphabetic characters (letters) are S_ALP.
    
    That's the situation when we reach the code block in sh_lex(),
    lines 917-970 in lex.c. From line 923 on, we're running that code
    for both S_ALP and S_DIG due to the fall-through. On line 923,
    endchar(lp) is } a.k.a. RBRACE, so that brings us to case RBRACE on
    line 944 and on. And there we notice that the mode switch to
    ST_BRACE only happens if the character was in the S_ALP category.
    If it's an S_DIG we default to the else clause and the correct code
    paths, including the necessary state table switches, don't happen.
    
    src/cmd/ksh93/sh/lex.c:
    - Make that mode switch happen when the variable name starts not
      only with a letter (S_ALP) but also with a digit (S_DIG).
    - The special handling to seek back to an initial '.' must remain
      limited to S_ALP (there is e.g. ${.sh.foo} but no ${.1.foo}).
    
    Resolves: https://github.com/ksh93/ksh/issues/907

--- src/cmd/ksh93/tests/variables.sh
+++ src/cmd/ksh93/tests/variables.sh
@@ -1275,6 +1275,49 @@ do	for word in '(word)' 'w(or)d' '(wor)d' 'w(ord)' 'w(ord' 'wor)d'
 	done
 done
 
+# Same again for positional parameters (minus the conditional assignment operators).
+# https://github.com/ksh93/ksh/issues/907
+
+set -- some_value  # $1 is set, $2 is unset
+
+for op in - :-
+do	for word in '(word)' 'w(or)d' '(wor)d' 'w(ord)' 'w(ord' 'wor)d'
+	do	got=$(set +x; eval "echo \${2${op}${word}}" 2>&1)
+		if	[[ $got != "$word" ]]
+		then	err_exit "\${2${op}${word}} when PP 2 is not set: expected \"$word\", got \"$got\""
+	        fi
+	done
+done
+
+for op in - :- \? :\?
+do	for word in '(word)' 'w(or)d' '(wor)d' 'w(ord)' 'w(ord' 'wor)d'
+	do	got=$(set +x; eval "echo \${1${op}${word}}" 2>&1)
+		if	[[ $got != "$1" ]]
+		then	err_exit "\${1${op}${word}} when PP 1 is set: expected \"$1\", got \"$got\""
+		fi
+	done
+done
+
+for op in + :+
+do	for word in '(word)' 'w(or)d' '(wor)d' 'w(ord)' 'w(ord' 'wor)d'
+	do	got=$(set +x; eval "echo \${2${op}${word}}" 2>&1)
+		if	[[ $got != "" ]]
+		then	err_exit "\${2${op}${word}} when PP 2 is not set: expected null, got \"$got\""
+		fi
+	done
+done
+
+for op in \? :\?
+do	for word in '(word)' 'w(or)d' '(wor)d' 'w(ord)' 'w(ord' 'wor)d'
+	do	got=$(set +x; eval "echo \${2${op}${word}}" 2>&1)
+		if	[[ $got != *": 2: $word" ]]
+		then	err_exit "\${2${op}${word}} when PP 2 is not set: expected *\": 2: $word\", got \"$got\""
+		fi
+	done
+done
+
+set --
+
 # ======
 # https://bugzilla.redhat.com/1147645
 case $'\n'$(env 'BASH_FUNC_a%%=() { echo test; }' "$SHELL" -c set) in
