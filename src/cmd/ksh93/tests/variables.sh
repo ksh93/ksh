147bb2048f5a2582c6ecab722ca7280e1448add1
Martijn Dekker <martijn@inlv.org>
2026-09-08 01:39:58 +0100

    Properly reinit PWD and ENV when executing a #!-less script
    Reproducer:
    
      $ echo 'PWD=$PWD' >script
      $ chmod +x script
      $ (unset PWD; ./script)
      PWD=
    
    When a script runs, variables that are not inherited from the
    environment should be reinitialised to their default states, and
    that definitely includes PWD containing the current working
    directory. The same bug applies to ENV which defaults to ~/.kshrc.
    
    This only happens when the script does not have a #! path, i.e. it
    is run after the shell forks, unwinds the context/longjmp stack,
    removing all state state that isn't main shell environment state,
    and then reinitialises via sh_reinit(), which is where the bug is.
    
    src/cmd/ksh93/sh/init.c:
    - Fix ENV reinit: move the code that gives ENV a default value from
      sh_init() to env_init(), which (as of 4d688150) is called from
      both sh_init() and sh_reinit().
    - Fix PWD reinit: in sh_reinit(), free sh.pwd and set it to NULL.
      When env_init then calls path_pwd, it will act as if initialising
      PWD for the first time: the inherited value (if any) is checked
      and used if good, otherwise the value is obtained with getcwd(3).
    
    src/cmd/ksh93/tests/variables.sh:
    - Tweak a couple of regression tests so they fail gracefully on
      93u+ 2012-08-01. This is unrelated but it allowed me to verify
      the crash failure mode of the new regression test.
    - Add new test that checks all the default variables that have
      default values: unset them all, then execute a #!-less script
      that checks they are all reinitialised with a value.

--- src/cmd/ksh93/tests/variables.sh
+++ src/cmd/ksh93/tests/variables.sh
@@ -1725,8 +1725,7 @@ got=$(set +x; { "$SHELL" -c '
 
 # ======
 # https://github.com/ksh93/ksh/issues/435
-"$SHELL" <<\EOF >/dev/null 2>&1; (((e=$?)==1)) || err_exit "getn/get discipline crash" \
-	"(expected status 1, got status $e$( ((e>128)) && print -n /SIG && kill -l "$e"))"
+"$SHELL" <<\EOF >/dev/null 2>&1
 unset nonexistent_var
 foo=nonexistent_var
 foo.getn() { :; }
@@ -1735,6 +1734,8 @@ unset -f foo.getn
 trap 'echo $((foo))' EXIT   # throw the echo $((foo)) 'unset parameter' error twice
 echo $((foo))
 EOF
+(((e=$?)==1)) || err_exit "getn/get discipline crash" \
+	"(expected status 1, got status $e$( ((e>128)) && print -n /SIG && kill -l "$e"))"
 
 # ======
 # exec after unset SHLVL
@@ -1773,7 +1774,7 @@ unset i
 
 exp=$'in main: MainShellValue\nin pathlocal: LocalValue'
 for v in IFS PATH SHELL FPATH CDPATH ENV
-do	got=$(eval "
+do	got=$(ulimit -c 0 2>/dev/null; eval "
 		function pathlocal
 		{
 			typeset $v=LocalValue
@@ -1814,6 +1815,18 @@ exp=6
 got=$(env 'BAD-NAME=ok' "$SHELL" -c 'i=0; while ((++i<=6)); do env; done' 2>&1 | grep -c '^BAD-NAME=ok$')
 [[ $got == "$exp" ]] || err_exit 'env var with invalid name not passed on to all child processes' \
 	"(expected $exp, got $got)"
+
+# ======
+# Unset predefined variables with default values were not reinitialised when executing a script without a #! path
+print -r "for var do
+	[[ -v \$var ]] || print -r \"\err_exit $((LINENO+1)) 'predefined variable \$var not reinitialised in executed script'\"
+done" >unset_execute_test
+chmod +x unset_execute_test
+{ errors=$("$SHELL" -xc 'unset "$@"; ./unset_execute_test "$@"' unset_execute_test \
+	PS2 IFS PWD SHELL MAILCHECK RANDOM ENV FCEDIT PS3 PPID _ TMOUT SECONDS LINENO OPTIND PS4 KSH_VERSION \
+	.sh.version .sh.file .sh.subshell .sh.lineno .sh.math .sh.pid .sh.ppid); } 2>/dev/null
+[[ e=$? -eq 0 ]] || err_exit "unset_execute_test crashes (got status $e$( ((e>128)) && print -n /SIG && kill -l "$e"))"
+eval "$errors"
 
 # ====== ADD NEW TESTS ABOVE THIS LINE ======
 # checks for tests run in parallel (see top)
