a20c94e0de2baabe6a1be8a8f583e9e0802eade3
Johnothan King <johnothanking@protonmail.com>
2026-09-11 19:43:23 -0700

    Fix crash when setting integer vars to multibyte chars (#1023)
    Reproducer for the crash that occurs in locale.sh under ASan:
       $ unset c
       $ integer c
       $ c=€
    
    [Martijn adds: The crash only occurs on systems where the C char
    type is signed. In that case, getchr() yields a negative value if
    the byte read has the 8th bit set, which is passed to the getop()
    macro, causing an out-of-bounds read.]
    
    src/cmd/ksh93/sh/streval.c:
    - Add (unsigned char*) typecasts to the getchr() and peekchr()
      macros to ensure they never yield a negative value.
    
    src/cmd/ksh93/tests/locale.sh:
    - Harden previously crashing strxfrm regression test by unsetting
      the c variable before using it.
    - Added a dedicated regression test that reproduces the crash in
      a spawned child shell.
    
    With this fix, all that remains of the ASan test failures are
    memory leaks (when using Linux on x86_64).
    
    Progresses https://github.com/ksh93/ksh/issues/518
    Co-authored-by: Martijn Dekker <martijn@inlv.org>

--- src/cmd/ksh93/sh/streval.c
+++ src/cmd/ksh93/sh/streval.c
@@ -83,8 +83,8 @@ typedef int        (*Math_2i_f)(Sfdouble_t,Sfdouble_t);
 typedef Sfdouble_t (*Math_3f_f)(Sfdouble_t,Sfdouble_t,Sfdouble_t);
 typedef int        (*Math_3i_f)(Sfdouble_t,Sfdouble_t,Sfdouble_t);
 
-#define getchr(vp)	(*(vp)->nextchr++)
-#define peekchr(vp)	(*(vp)->nextchr)
+#define getchr(vp)	(*(unsigned char*)((vp)->nextchr++))
+#define peekchr(vp)	(*(unsigned char*)((vp)->nextchr))
 #define ungetchr(vp)	((vp)->nextchr--)
 
 /*
